268307ec56b68e6a607655e4f98ce0cba14141cd
joaopedroa <joaopedroa.6@hotmail.com>
2021-03-25 13:58:25 -0300

    Zuul autorização

---
+++ hr-api-gateway-zuul/src/main/java/com/springcloud/hrapigatewayzuul/config/AppConfig.java
@@ -0,0 +1,23 @@
+package com.springcloud.hrapigatewayzuul.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+public class AppConfig {
+	
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+		tokenConverter.setSigningKey("MY-SECRET-KEY");
+		return tokenConverter;
+	}
+	
+	// Responsável por ler as informações do token
+	@Bean
+	public JwtTokenStore tokenStore() {
+		return new JwtTokenStore(accessTokenConverter());
+	}
+}
