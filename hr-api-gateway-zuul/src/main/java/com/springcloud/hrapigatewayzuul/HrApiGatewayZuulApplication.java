f364b41d74ad1ae326fb2f6a3731cc822d5e90b3
joaopedroa <joaopedroa.6@hotmail.com>
2021-03-25 16:00:10 -0300

    Colocando security configs no git de configuracao

--- hr-api-gateway-zuul/src/main/java/com/springcloud/hrapigatewayzuul/HrApiGatewayZuulApplication.java
+++ hr-api-gateway-zuul/src/main/java/com/springcloud/hrapigatewayzuul/HrApiGatewayZuulApplication.java
@@ -1,5 +1,7 @@
 package com.springcloud.hrapigatewayzuul;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -9,9 +11,13 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableZuulProxy
 @SpringBootApplication
 public class HrApiGatewayZuulApplication {
+	
+	@Value("${jwt.secret}")
+	private String teste;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrApiGatewayZuulApplication.class, args);
 	}
+
 
 }
